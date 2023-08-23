e8c05ad5535d765a626aa28f6a5d84e0c6b6b714
techbywes <dhspotless@gmail.com>
2023-08-23 16:20:56 +0100

    updated another commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate
+        velit autem voluptatibus voluptates nemo unde id doloremque, ratione
+        obcaecati rem est porro molestiae aut ipsum quos dolorem nulla ullam
+        odio!
+      </p>
     </div>
   );
 }
