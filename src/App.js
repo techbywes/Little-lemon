b1fc92e201240ccbe7562242741a2c24932b304e
techbywes <dhspotless@gmail.com>
2023-08-28 12:00:01 +0100

    updated comfrimation page and home page

--- src/App.js
+++ src/App.js
@@ -2,19 +2,18 @@ import "./App.css";
 import Home from "./Home";
 import Booking from "./Booking";
 import ComfirmationPage from "./ComfirmationPage";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/Home" element={<Home />} />
-          <Route path="/Booking" element={<Booking />} />
-          <Route path="/ComfirmationPage" element={<ComfirmationPage />} />
-        </Routes>
-      </Router>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Home" element={<Home />} />
+        <Route path="/Booking" element={<Booking />} />
+        <Route path="/ComfirmationPage" element={<ComfirmationPage />} />
+      </Routes>
+    </Router>
   );
 }
 
